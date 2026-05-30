00f2ee2ded35da6a9138ddcbec5fbf8369af928a
Vlasta Vašut <vasutvlastimil@gmail.com>
2026-05-30 13:35:04 +0200

    adding data to bazar arr

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,3 +1,24 @@
 export const nav:Array<string> = ['bazar', 'seznamka', 'facebook', 'threads', 'youtube', 'medialní web'];
 
-export const bazar_nav:Array<string> = ['vše', 'auto', 'mobil', 'nábytek', 'elektronika', 'nájem', 'neshoda', 'nečeština', 'ostatní'];
+export const bazar_nav:Array<string> = ['vše', 'auto', 'kolo', 'mobil', 'nábytek', 'elektronika', 'nájem', 'neshoda', 'nečeština', 'ostatní'];
+
+export const bazar = [
+    {img:'ALU-kola', type:'auto'},
+    {img:'CD_kalhoty_flek_na_zadku', type:'ostatní'},
+    {img:'Dusan', type:'neshoda'},
+    {img:'LG_velvet_128GB', type:'mobil'},
+    {img:'Wolcvagen', type:'auto'},
+    {img:'apple_watch_udelet', type:'elektronika'},
+    {img:'berlingo_s_velkyma_svetlana', type:'auto'},
+    {img:'chcikoupitvolm', type:'nečeština'},
+    {img:'daruji_gauc_z_Ikei', type:'nábytek'},
+    {img:'dobri_vecer_za_58000kc', type:'ostatní'},
+    {img:'dotykovi_mobyl_redmi_9A', type:'mobil'},
+    {img:'el_kolo_kradene', type:'kolo'},
+    {img:'herni_miska', type:'nečeština'},
+    {img:'hibi_enom_zetes', type:'nečeština'},
+    {img:'hledam_pronajem_pes', type:'nájem'},
+    {img:'horske_kolo_maso', type:'neshoda'},
+    {img:'hruzneznackyovladace', type:'elektronika'},
+    {img:'iphone_hell', type:'neshoda'},
+];
